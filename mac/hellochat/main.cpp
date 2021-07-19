672d56e23f6e319485ad82c4c70676fef764ea31
xmcy0011 <xmcy0011@sina.com>
2021-07-19 20:23:05 +0800

    实现登录demo

--- mac/hellochat/main.cpp
+++ mac/hellochat/main.cpp
@@ -1,11 +1,17 @@
-#include "mainwindow.h"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+#include "logindialog.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+
+  LoginDialog login;
+  if (login.exec() == QDialog::Accepted) {
     MainWindow w;
     w.show();
     return a.exec();
+  } else {
+    return 0;
+  }
 }
