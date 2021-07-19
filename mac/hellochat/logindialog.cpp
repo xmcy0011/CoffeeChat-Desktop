551f321fe068f9bab2573bde25ae1512d481f3f6
xmcy0011 <xmcy0011@sina.com>
2021-07-19 20:56:44 +0800

    登录界面实现

---
+++ mac/hellochat/logindialog.cpp
@@ -0,0 +1,10 @@
+#include "logindialog.h"
+
+#include "ui_logindialog.h"
+
+LoginDialog::LoginDialog(QWidget *parent)
+    : QDialog(parent), ui(new Ui::LoginDialog) {
+  ui->setupUi(this);
+}
+
+LoginDialog::~LoginDialog() { delete ui; }
